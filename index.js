9608435199ef9131672cec92bcc6f0a5b0815593
chancocknz <callum.hancock.nz@gmail.com>
2023-10-10 12:19:29 +1300

    Changing port numbers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = 80;
 
 app.use(
   cors({
